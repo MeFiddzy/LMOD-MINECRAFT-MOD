4715572cebbc4d8f4eb941a47ea889465a889a46
MeFiddzy <fiddzy.pg@gmail.com>
2025-06-20 21:33:09 +0300

    E

--- src/main/java/com/mefiddzy/lmod/item/ModToolTiers.java
+++ src/main/java/com/mefiddzy/lmod/item/ModToolTiers.java
@@ -5,7 +5,7 @@ import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.SimpleTier;
 
-public class ModToolTiers {
+public class  ModToolTiers {
     public static final Tier ENPOWERED_GOLD = new SimpleTier(ModTags.Blocks.INCORRECT_FOR_ENPOWERED_GOLD_TOOL,
             3019, 12.3f, 5.0f, 29, () -> Ingredient.of(ModItems.ENPOWERED_GOLD_INGOT));
 
